f0c3dea7a4b4aeaad9acc723f142a23139a7d6bb
15017199 <15017199@myrp.edu.sg>
2017-04-28 12:06:35 +0800

    - 1

--- app/src/main/java/sg/edu/rp/c347/demolistview/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c347/demolistview/MainActivity.java
@@ -2,12 +2,24 @@ package sg.edu.rp.c347.demolistview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    ListView lvFood;
+
+    ArrayList<String>alColours = new ArrayList<String>();
+    ArrayAdapter<String> aaFood;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        lvFood = (ListView)findViewById(R.id.lvFood);
+        lvFood.setAdapter(aaFood);
+
+
     }
 }
